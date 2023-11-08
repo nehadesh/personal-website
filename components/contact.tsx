ffd338740461d19f619fccf6430242e0c9d19684
Neha Deshpande <neha.n.deshpande@gmail.com>
2023-11-08 10:53:32 -0800

    updates

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,12 +7,12 @@ import { motion } from "framer-motion";
 import { sendEmail } from "@/actions/sendEmail";
 
 export default function Contact() {
-  const { ref } = useSectionInView("Contact");
+  //   const { ref } = useSectionInView("Contact");
 
   return (
     <motion.section
       id="contact"
-      ref={ref}
+      //   ref={ref}
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center justify-center scroll-mt-[18rem]"
       initial={{ opacity: 0 }}
       whileInView={{
